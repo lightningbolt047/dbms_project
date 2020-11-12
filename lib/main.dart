01bd53d1f4b84e46e4b03745b6abdcc89d5f9aad
Sashank Visweshwaran <sashankvisweshwaran@gmail.com>
2020-11-12 11:40:07 +0530

    New UI for login screen (main.dart) and fixed login logic

--- lib/main.dart
+++ lib/main.dart
@@ -30,113 +30,136 @@ class _LoginState extends State<Login> {
       theme: ThemeData(fontFamily: screenHeadFont),
       home: Builder(
         builder:(context) => Scaffold(
-          appBar: AppBar(
-            title: Text("Login Page"),
-            backgroundColor: Colors.blueAccent,
-          ),
+          // appBar: AppBar(
+          //   title: Text("Login Page"),
+          //   backgroundColor: Colors.blueAccent,
+          // ),
           body: Padding(
             padding: const EdgeInsets.symmetric(horizontal:50.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
+            child: Row(
               children: [
-                //TODO Hero Animations once other jobs are complete
-                Text(
-                  "Enter your username and password here",
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w800,
+                Expanded(
+                  flex: 2,
+                  child: Padding(
+                    padding: EdgeInsets.all(8),
+                    child: Card(
+                      child: Padding(
+                        padding: EdgeInsets.all(8),
+                        child: Column(
+                          children: [],
+                        ),
+                      ),
+                    ),
                   ),
                 ),
-                SizedBox(
-                  height: 30,
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: "Enter your username",
-                  ),
-                  onChanged: (string){
-                    setState(() {
-                      _inputUsername=string;
-                      evalStatus=userEvalStatusTypes.firstAttempt;
-                    });
-                  },
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: "Enter your password",
-                  ),
-                  onChanged: (string){
-                    setState(() {
-                      _inputPassword=string;
-                      evalStatus=userEvalStatusTypes.firstAttempt;
-                    });
-                  },
-                  obscureText: true,
-                ),
-                SizedBox(
-                  height:30,
-                ),
-                LoginButton(() async{
-                  RequestServer server= RequestServer();
-                  server.setAction("select usertype,ID from UserTable where username=$_inputUsername");
-                  server.setQtype("R");
-                  bool userPresent= await server.checkUserPresence(_inputUsername);
-                  bool correctCredentials=await server.checkCredentials(_inputUsername, _inputPassword);
-                  var response=await server.getDecodedResponse();
-                  setState(() {
-                    if(_inputUsername==null || _inputPassword==null){
-                      evalStatus=userEvalStatusTypes.missingCredentials;
-                    }
-                    // else{   //AB testing else from this point
-                    //   if(userPresent){
-                    //     if(correctCredentials){
-                    //       String usertype=response[0]['usertype'];
-                    //       String ID=response[0]['ID'];
-                    //       if(usertype=="employee"){
-                    //         Navigator.push(
-                    //                     context,
-                    //                     MaterialPageRoute(builder: (context) => EmployeeUniqueScreen(_inputUsername,ID,pageTypeList.employee)
-                    //                     )
-                    //                 );
-                    //       }
-                    //       else if(usertype=="outlet"){
-                    //         Navigator.push(
-                    //             context,
-                    //             MaterialPageRoute(builder: (context) => OutletUniqueScreen(ID)
-                    //             )
-                    //         );
-                    //       }
-                    //       else if(usertype=="employeemanager"){
-                    //         Navigator.push(
-                    //             context,
-                    //             MaterialPageRoute(builder: (context) => EmployeeManagerScreen(_inputUsername)
-                    //             )
-                    //         );
-                    //       }
-                    //       else if(usertype=="outletmanager"){
-                    //         Navigator.push(
-                    //             context,
-                    //             MaterialPageRoute(builder: (context) => MultiManagerScreen(pageTypeList.outletManager,_inputUsername)
-                    //             )
-                    //         );
-                    //       }
-                    //       else if(usertype=="procurementmanager"){
-                    //         Navigator.push(
-                    //             context,
-                    //             MaterialPageRoute(builder: (context) => MultiManagerScreen(pageTypeList.procurementManager,_inputUsername)
-                    //             )
-                    //         );
-                    //       }
-                    //     }
-                    //     else{
-                    //       evalStatus=userEvalStatusTypes.wrongCredentials;
-                    //     }
-                    //   }
-                    //   else{
-                    //     evalStatus=userEvalStatusTypes.userDNE;
-                    //   }
-                    // }
+                Expanded(
+                  flex: 1,
+                  child:Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Container(
+                            width: double.maxFinite,
+                            height: 50,
+                            child: Center(
+                              child: Text("Login",style: TextStyle(
+                                  color: Colors.blueAccent,
+                                  fontSize: 25
+                              ),),
+                            ),
+                          ),
+                          TextField(
+                            decoration: InputDecoration(
+                              labelText: "Enter your username",
+                            ),
+                            onChanged: (string){
+                              setState(() {
+                                _inputUsername=string;
+                                evalStatus=userEvalStatusTypes.firstAttempt;
+                              });
+                            },
+                          ),
+                          TextField(
+                            decoration: InputDecoration(
+                              labelText: "Enter your password",
+                            ),
+                            onChanged: (string){
+                              setState(() {
+                                _inputPassword=string;
+                                evalStatus=userEvalStatusTypes.firstAttempt;
+                              });
+                            },
+                            obscureText: true,
+                          ),
+                          sizedBoxInColumn,
+                          LoginButton(() async{
+                            RequestServer server= RequestServer();
+                            server.setAction("select usertype,ID from UserTable where username=$_inputUsername");
+                            server.setQtype("R");
+                            if(_inputUsername==null || _inputPassword==null){
+                              setState(() {
+                                evalStatus=userEvalStatusTypes.missingCredentials;
+                              });
+                              return;
+                            }
+                            bool userPresent= await server.checkUserPresence(_inputUsername);
+                            bool correctCredentials=await server.checkCredentials(_inputUsername, _inputPassword);
+                            var response=await server.getDecodedResponse();
+                            setState(() {
+                              // else{   //AB testing else from this point
+                              //   if(userPresent){
+                              //     if(correctCredentials){
+                              //       String usertype=response[0]['usertype'];
+                              //       String ID=response[0]['ID'];
+                              //       if(usertype=="employee"){
+                              //         Navigator.push(
+                              //                     context,
+                              //                     MaterialPageRoute(builder: (context) => EmployeeUniqueScreen(_inputUsername,ID,pageTypeList.employee)
+                              //                     )
+                              //                 );
+                              //       }
+                              //       else if(usertype=="outlet"){
+                              //         Navigator.push(
+                              //             context,
+                              //             MaterialPageRoute(builder: (context) => OutletUniqueScreen(ID)
+                              //             )
+                              //         );
+                              //       }
+                              //       else if(usertype=="employeemanager"){
+                              //         Navigator.push(
+                              //             context,
+                              //             MaterialPageRoute(builder: (context) => EmployeeManagerScreen(_inputUsername)
+                              //             )
+                              //         );
+                              //       }
+                              //       else if(usertype=="outletmanager"){
+                              //         Navigator.push(
+                              //             context,
+                              //             MaterialPageRoute(builder: (context) => MultiManagerScreen(pageTypeList.outletManager,_inputUsername)
+                              //             )
+                              //         );
+                              //       }
+                              //       else if(usertype=="procurementmanager"){
+                              //         Navigator.push(
+                              //             context,
+                              //             MaterialPageRoute(builder: (context) => MultiManagerScreen(pageTypeList.procurementManager,_inputUsername)
+                              //             )
+                              //         );
+                              //       }
+                              //     }
+                              //     else{
+                              //       evalStatus=userEvalStatusTypes.wrongCredentials;
+                              //     }
+                              //   }
+                              //   else{
+                              //     evalStatus=userEvalStatusTypes.userDNE;
+                              //   }
+                              // }
 
 //The above code is actually the one we must use but its commented because, the DB is just not ready yet
 
@@ -146,25 +169,30 @@ class _LoginState extends State<Login> {
 
 
 
-                    else if(_inputUsername=='sas047' /*Dummy condition, irl, we'll check if _inputUsername exists in the table*/){
-                      if(_inputUsername=='sas047' && _inputPassword=='hello' /*Dummy condition! IRL, we'll check if _inputUsername matches _inputPassword*/){
-                        evalStatus=userEvalStatusTypes.evalSuccess;
-                        //Below is a dummy call for a new activity. IRL, use if-else to determine activity type for corresponding usertype
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => EmployeeManagerScreen(_inputUsername)
-                            )
-                        );
-                      }
-                      else{
-                        evalStatus=userEvalStatusTypes.wrongCredentials;
-                      }
-                    }
-                    else{
-                      evalStatus=userEvalStatusTypes.userDNE;
-                    }
-                  });
-                },evalStatus),
+                              if(_inputUsername=='sas047' /*Dummy condition, irl, we'll check if _inputUsername exists in the table*/){
+                                if(_inputUsername=='sas047' && _inputPassword=='hello' /*Dummy condition! IRL, we'll check if _inputUsername matches _inputPassword*/){
+                                  evalStatus=userEvalStatusTypes.evalSuccess;
+                                  //Below is a dummy call for a new activity. IRL, use if-else to determine activity type for corresponding usertype
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(builder: (context) => EmployeeManagerScreen(_inputUsername)
+                                      )
+                                  );
+                                }
+                                else{
+                                  evalStatus=userEvalStatusTypes.wrongCredentials;
+                                }
+                              }
+                              else{
+                                evalStatus=userEvalStatusTypes.userDNE;
+                              }
+                            });
+                          },evalStatus),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),),
               ],
             ),
           ),
